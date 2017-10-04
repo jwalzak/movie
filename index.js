6ecde0bb7c1a3b25080d162194dcf1e64e29c814
Jason Walzak <walzak@gmail.com>
2017-10-03 20:44:34 -0400

    the application now does POST when clicking submit installed the body parser npm package to get POST value Updated pug file to have a form tag commented the index.js file

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const dotenv = require('dotenv')
 const _ = require('lodash')
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const pug = require('pug')
 const firebase = require('firebase')
 
+// Calls dotenv to keep api keys secret
 dotenv.load()
 
+// Tells pug where to find the template file
 const compiledFile = pug.compileFile('./pug/template.pug')
 
+// Firebase api information
 const config = {
   apiKey: process.env.API_KEY,
   authDomain: process.env.AUTH_DOMAIN,
@@ -20,12 +24,27 @@ const config = {
 
 firebase.initializeApp(config)
 
+// For parsing input data through post
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+
+app.use(bodyParser.json())
+
+// When the page loads this is the get
 app.get('/', function(req, res) {
   res.send(compiledFile({
     name: 'Jason'
   }))
 })
 
+// When the user hits submit this is the function that makes it work
+app.post('/', function(req, res){
+  res.send(req.body)
+})
+
+
+// Starts the server
 app.listen(3000, function() {
   console.log('Listening on port 3000')
 })
